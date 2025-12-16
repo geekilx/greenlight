1bdb372fbab2ce641ef6eee3dd2a1da0c32bdef6
ilx <iliakeshavarz23@gmail.com>
2025-12-16 17:11:45 +0330

    creating a method for users models in order to get users by token and do an innrer join on users.id and tokens.user_id

--- internal/data/users.go
+++ internal/data/users.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"crypto/sha256"
 	"database/sql"
 	"errors"
 	"time"
@@ -11,7 +12,7 @@ import (
 )
 
 type User struct {
-	ID        int       `json:"id"`
+	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
@@ -100,7 +101,7 @@ func (m *UserModel) GetByEmail(email string) (*User, error) {
 }
 
 func (m *UserModel) Update(user *User) error {
-	stmt := `UPDATE users SET name = $1, email $2, password_hash = $3, activated = $4, version = version + 1 WHERE id = $5 AND version = $6 RETURNING version`
+	stmt := `UPDATE users SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1 WHERE id = $5 AND version = $6 RETURNING version`
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
@@ -121,6 +122,37 @@ func (m *UserModel) Update(user *User) error {
 
 	return nil
 
+}
+
+func (m *UserModel) GetForToken(scope, token string) (*User, error) {
+
+	tokenHash := sha256.Sum256([]byte(token))
+
+	stmt := `SELECT users.id, users.created_at, users.name, users.email, users.password_hash, users.activated, users.version
+FROM users
+INNER JOIN tokens
+ON users.id = tokens.user_id
+WHERE tokens.hash = $1
+AND tokens.scope = $2
+AND tokens.expiry > $3`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	args := []any{tokenHash[:], scope, time.Now()}
+
+	var user User
+	err := m.DB.QueryRowContext(ctx, stmt, args...).Scan(&user.ID, &user.CreatedAt, &user.Name, &user.Email, &user.Password.hash, &user.Activated, &user.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
 }
 
 func ValidateUser(v *validator.Validator, user *User) {
