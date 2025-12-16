e7f44eb2f8516e79eef33915fb40e2b590707b9c
ilx <iliakeshavarz23@gmail.com>
2025-12-16 17:07:24 +0330

    creating token methods for inserting token to tokens table in postgres and validation for tokens

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,11 +1,14 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
 	"database/sql"
 	"encoding/base32"
 	"time"
+
+	"greenlight.ilx.net/internal/validator"
 )
 
 const (
@@ -41,9 +44,54 @@ func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error
 
 	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
 
+	// hashing the token by sha256 because it is much fatser than bcrypt
 	hash := sha256.Sum256([]byte(token.Plaintext))
 	token.Hash = hash[:]
 
 	return token, nil
 
+}
+
+func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token, err := generateToken(userID, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = m.Insert(token)
+	if err != nil {
+		return nil, err
+	}
+
+	return token, err
+
+}
+
+func (m TokenModel) Insert(token *Token) error {
+
+	stmt := `INSERT INTO tokens (hash, user_id, expiry, scope) VALUES($1, $2, $3, $4)`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	args := []any{token.Hash, token.UserID, token.Expiry, token.Scope}
+
+	_, err := m.DB.ExecContext(ctx, stmt, args...)
+	return err
+}
+
+func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	stmt := `DELETE FROM tokens WHERE user_id = $1 AND scope = $2`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(ctx, stmt, userID, scope)
+	return err
+
+}
+
+func ValidateTokenPlaintext(v *validator.Validator, tokenPlaintext string) {
+	v.Check(tokenPlaintext != "", "token", "must be provided")
+	v.Check(len(tokenPlaintext) == 26, "token", "must be 26 bytes long")
 }
